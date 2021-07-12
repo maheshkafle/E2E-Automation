986b0f618d8b37b98e86fd5c23fd20033e08ef5c
kaflemahesh <mahesh.kafle@smarsh.com>
2021-07-13 00:17:35 +0545

    Eliminated extra import statements

--- Tests/test_reappearing_element.py
+++ Tests/test_reappearing_element.py
@@ -1,9 +1,6 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import ActionChains
 import time
 
